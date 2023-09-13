98df8824221d676740bdba457bb59bf9ef04714b
liuzel01 <liuzelin01@gmail.com>
2023-09-13 22:00:21 +0800

    feat: 提交远程

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // resolve: {
-  //   alias: {
-  //     // 配置路径别名
-  //     "@": fileURLToPath(new URL("./src", import.meta.url)),
-  //     // "@img": fileURLToPath(new URL("./src/assets/img", import.meta.url)),
-  //   },
-  // }
+  // 增加以下的配置
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      },
+      {
+        find: 'components',
+        replacement: resolve(__dirname, 'src/components')
+      }
+    ]
+  }
 })
